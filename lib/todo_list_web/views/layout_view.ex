bca0287bdeaf9761b74bce97e3b43e1334ca8cef
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-07-04 15:27:08 +0100

    :sunrise:

---
+++ lib/todo_list_web/views/layout_view.ex
@@ -0,0 +1,7 @@
+defmodule TodoListWeb.LayoutView do
+  use TodoListWeb, :view
+
+  # Phoenix LiveDashboard is available only in development by default,
+  # so we instruct Elixir to not warn if the dashboard route is missing.
+  @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+end
